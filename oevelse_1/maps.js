a987ec561175982e084218674409dcb57313a862
Frederikke Holmen <frederikkeholmen@Frederikkes-MacBook-Pro.local>
2017-03-04 14:26:50 +0100

    json del 1

--- oevelse_1/maps.js
+++ oevelse_1/maps.js
@@ -1,1 +1,30 @@
+function initMap() {
 
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 10,
+        center: {
+            lat: 55.682468,
+            lng: 12.613122
+        }
+    });
+
+
+
+    console.log("JsonHentet");
+    $.getJSON('maps.json', dataHentet);
+}
+
+function dataHentet(data) {
+    console.log("dataHentet");
+    //depugInfo("Loadet data");
+    data.forEach(createMarker);
+
+}
+
+function createMarker(mapinfo) {
+    var marker = new google.maps.Marker({
+        position: mapinfo.position,
+        map: map,
+        title: mapinfo.navn
+    });
+}
